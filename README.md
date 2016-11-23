ef8ca3c312ee0b84f5c2846d3650709ceb3bace7
Adam Schønemann <adamschoenemann@gmail.com>
2016-11-23 15:17:17 +0100

    some analysis works, but while is teasing us

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Analysis Framework
+For Automated Software Analysis at ITU 2016.
+
+## TODO:
+- The `While` statement is not working. Consider maybe implementing confluence
+  points explicitly in the control-flow graph. Should also simplify the code
+  alot in `compute`.
--- src/Avail.hs
+++ src/Avail.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Avail where
 
@@ -18,7 +19,19 @@ data Expr
   | Const Int
   | Var String
   | Input
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord)
+
+ppExpr expr = case expr of
+  Add e1 e2 -> ppExpr e1 ++ " + " ++ ppExpr e2
+  Sub e1 e2 -> ppExpr e1 ++ " - " ++ ppExpr e2
+  Mul e1 e2 -> ppExpr e1 ++ " * " ++ ppExpr e2
+  Gt  e1 e2 -> ppExpr e1 ++ " > " ++ ppExpr e2
+  Const x   -> show x
+  Var n     -> n
+  Input     -> "input"
+
+instance Show Expr where
+  show = ppExpr
 
 data Stmt
   = Skip
@@ -26,7 +39,19 @@ data Stmt
   | ITE Expr Stmt Stmt
   | Comp Stmt Stmt
   | While Expr Stmt
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord)
+
+ppStmt n stmt = case stmt of
+  Skip -> "skip"
+  Ass v e -> v ++ " := " ++ ppExpr e
+  ITE e s1 s2 -> "if " ++ ppExpr e ++ " then " ++ ppStmt n s1 ++ " else " ++ ppStmt n s2
+  Comp s1 s2 -> let ident = replicate n '\t' in ppStmt n s1 ++ ";\n" ++ ident ++ ppStmt n s2
+  While e s  -> let ident = replicate (n+1) '\t'
+                in  "while (" ++ ppExpr e ++ ") do (\n" ++ ident ++ ppStmt (n+1) s ++ ";\n)"
+
+instance Show Stmt where
+  show = ppStmt 0
+
 
 comp2list :: Stmt -> [Stmt]
 comp2list (Comp s1 c2@(Comp s2 s3)) = s1 : comp2list c2
@@ -83,10 +108,10 @@ data CFGNode = Cond Expr | CFGStmt Stmt deriving (Show)
 prog1 :: Stmt
 prog1 =
   Ass "x" (Var "a" `Mul` Var "b") `Comp`
-  -- (ITE ((Var "a" `Mul` Var "b") `Gt` (Const 20 `Mul` Var "c"))
-  --      (Ass "y" (Const 20 `Add` Var "a"))
-  --      (Ass "y" (Const 30 `Add` Var "c"))
-  -- ) `Comp`
+  (ITE ((Var "a" `Mul` Var "b") `Gt` (Const 20 `Mul` Var "c"))
+       (Ass "y" (Const 20 `Add` Var "a"))
+       (Ass "y" (Const 30 `Add` Var "c"))
+  ) `Comp`
   Ass "z" (Const 20 `Mul` Const 30) `Comp`
   Ass "a" (Const 20) `Comp`
   Ass "u" (Var "a" `Mul` Var "b")
@@ -130,32 +155,32 @@ type CFGState a = State (ID, [Node CFGNode], Edges) a
 
 cfg :: Stmt -> CFG CFGNode
 cfg s =
-  let (_,(sink, ns, es)) = runState (compute s) (0, [], [])
+  let (_,(sink, ns, es)) = runState (computeCFG s) (0, [], [])
   in  CFG { nodes = fromNodes ns, edges = S.fromList $ es, src = 0, sink = sink } where
     fromNodes nodes = M.fromList $ map (\(Node i n) -> (i, Node i n)) nodes
 
-compute :: Stmt -> CFGState [ID]
-compute s' =
+computeCFG :: Stmt -> CFGState [ID]
+computeCFG s' =
   case s' of
     Skip -> (:[]) . fst' <$> get
     Ass v e -> (:[]) <$> newNode (CFGStmt $ Ass v e)
     ITE e tr fl -> do
       condi <- newNode (Cond e) -- cond index
       -- FIXME: This will crash for nested ifs?
-      [tri] <- compute tr -- true index
-      [fli] <- compute fl -- false index
-      newEdge condi tri
-      newEdge condi fli
-      return [fli,tri]
+      trids <- computeCFG tr -- true index
+      flids <- computeCFG fl -- false index
+      mapM (newEdge condi) trids
+      mapM (newEdge condi) flids
+      return $ flids ++ trids
     Comp s1 s2 -> do
-      u <- compute s1
+      u <- computeCFG s1
       let v = (maximum u) + 1
       mapM (flip newEdge $ v) u
-      compute s2
+      computeCFG s2
     While e s -> do
       u <- newNode (Cond e)
       newEdge u (u+1)
-      v <- compute s
+      v <- computeCFG s
       mapM (flip newEdge $ u) v
       return v -- return ref to cond
   where
@@ -171,32 +196,54 @@ compute s' =
       (i, ns, es) <- get
       put (i, ns, (u,v):es)
 
+data ProgPoint =
+  PP { dependent :: Set ID, node :: CFGNode }
+     deriving (Show)
+
+cfgToProgP :: CFG CFGNode -> [ProgPoint]
+cfgToProgP (CFG {nodes, edges, src, sink}) =
+  map nodeToProgP (M.elems nodes) where
+    nodeToProgP node@(Node i inner) =
+      let dependent = incoming i
+      in  PP { dependent = dependent, node = inner }
+    incoming i = S.map fst . S.filter ((i == ) . snd) $ edges
 
 
+type Equation = [Lattice] -> Lattice
+
+progPsToEqs :: [ProgPoint] -> [Equation]
+progPsToEqs points = map pointToEq points where
+  leastUpperBound :: [Lattice] -> Set ID -> Lattice
+  leastUpperBound prev deps
+    | S.null deps = S.empty
+    | otherwise = foldl1 S.intersection . map (\d -> prev !! d) . S.toList $ deps
+  pointToEq (PP { dependent, node }) prev =
+    case node of
+      Cond expr     -> avail expr $ leastUpperBound prev dependent
+      CFGStmt stmt  -> stmtToTFun stmt $ leastUpperBound prev dependent
+
+type BigT = [Lattice] -> [Lattice]
+
+eqsToBigT :: [Equation] -> BigT
+eqsToBigT eqs l = map ($ l) eqs
+
+solveFix :: [Lattice] -> BigT -> [Lattice]
+solveFix l bigT =
+  let l' = bigT l
+  in  if (l == l') then l else solveFix l' bigT
+
+collectExprs :: Stmt -> Set Expr
+collectExprs stmt = case stmt of
+  Skip -> S.empty
+  Ass _ e -> exprs e
+  ITE e t f -> exprs e `union` collectExprs t `union` collectExprs f
+  Comp s1 s2 -> collectExprs s1 `union` collectExprs s2
+  While e s -> exprs e `union` collectExprs s
 
 
-
-
-
-
-
-
-
-
--- data ProgPoint =
---   PP { incoming :: [Int], stmt :: Stmt }
-
-
-
-
-
--- -- no, this won't work. We need to create the control flow graph :()
--- progToTFun :: Stmt -> [[Lattice] -> Lattice]
--- progToTFun stmt' = go 0 stmt' where
---   go :: Int -> Stmt -> [[Lattice] -> Lattice]
---   go i stmt = case stmt of
---     (Skip)       -> [id . (!! i)]
---     (Ass v e)    -> [assign v e . (!! i)]
---     (ITE e s s') -> [avail e . (!! i)] ++ go (i+1) s ++ go (i+2) s'
---     (Comp s s')  -> go i s ++ go (i+1) s'
---     (While e s)  -> [avail e . (!! i)] ++ go (i+1) s -- least upper bound here?
+analyzeProg :: Stmt -> [Lattice]
+analyzeProg prog = solveFix initial bigT where
+  allExprs = collectExprs prog
+  progps = cfgToProgP $ cfg prog
+  bigT = eqsToBigT . progPsToEqs $ progps
+  initial = replicate (length progps) allExprs
